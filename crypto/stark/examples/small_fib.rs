12cae72d609483a9158bf1e2d0a1786461ce2637
AnhMinh <anhminh@seznam.cz>
2021-08-13 14:54:19 +0200

    Alter the small_fib example.

--- crypto/stark/examples/small_fib.rs
+++ crypto/stark/examples/small_fib.rs
@@ -1,8 +1,9 @@
 #![warn(clippy::all)]
+use env_logger;
 use log::info;
 use std::time::Instant;
 use zkp_macros_decl::field_element;
-use zkp_primefield::{FieldElement, Root};
+use zkp_primefield::{FieldElement, Root, One};
 use zkp_stark::{Constraints, Provable, RationalExpression, TraceTable, Verifiable};
 use zkp_u256::U256;
 
@@ -38,7 +39,7 @@ impl Verifiable for Claim {
             (Trace(0, 0) - 1) * on_row(0),
             (Trace(0, 0) - &self.value) * on_row(self.index),
         ])
-        .unwrap()
+            .unwrap()
     }
 }
 
@@ -61,16 +62,32 @@ fn main() {
 
     info!("Constructing claim");
     let claim = Claim {
-        index: 1000,
-        value: field_element!("0142c45e5d743d10eae7ebb70f1526c65de7dbcdb65b322b6ddc36a812591e8f"),
+        // index: 1000,
+        // value: field_element!("0142c45e5d743d10eae7ebb70f1526c65de7dbcdb65b322b6ddc36a812591e8f"),
+        index : 2,
+        value: field_element!("01")
     };
     info!("Claim: {:?}", claim);
 
     info!("Constructing witness");
     let witness = Witness {
-        secret: field_element!("cafebabe"),
+        // secret: field_element!("cafebabe"),
+        secret: field_element!("00"),
     };
     info!("Witness: {:?}", witness);
+
+    let mut cx = FieldElement::one();
+    for n in 1..1001 {
+        let claim2 = Claim {
+            index: 5,
+            value: cx.clone(),  // or: FieldElement::from_uint(&U256::from(n))
+        };
+        if claim2.check(&witness) == Ok(()) {
+            println!("Found the match {:?}, {:?}", n, cx);
+            break;
+        }
+        cx += FieldElement::one();
+    }
 
     assert_eq!(claim.check(&witness), Ok(()));
 
