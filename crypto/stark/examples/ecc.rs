f973360f33ab0c2c83e244fe72ddbfb55335e098
AnhMinh <anhminh@seznam.cz>
2021-08-18 13:39:27 +0200

    Add more examples of points on curve. (Actually results after n doubling of the first commented point.)

--- crypto/stark/examples/ecc.rs
+++ crypto/stark/examples/ecc.rs
@@ -138,7 +138,7 @@ impl Provable<&Witness> for NDoublingPointClaim{
             let y = &trace[(i, 1)].clone();
             let m = ((x + x + x) * x + FieldElement::one()) / (y + y);
             // info!("m is {:?}", &m);
-            trace[(i, 2)] = ((x + x + x) * x + FieldElement::one());
+            trace[(i, 2)] = (x + x + x) * x + FieldElement::one();
             trace[(i, 3)] = y + y;
             trace[(i, 4)] = m.clone();
             trace[(i, 5)] = m.clone() * m.clone() - x - x;
@@ -265,18 +265,38 @@ fn point_on_curve_construction(){
     // From algebra/elliptic-curve/src/curve.rs test_double
     info!("Constructing claim");
     let claim = PointOnCurveClaim {
-        x: field_element!("01ef15c18599971b7beced415a40f0c7deacfd9b0d1819e03d723d8bc943cfca"),
-        y: field_element!("005668060aa49730b7be4801df46ec62de53ecd11abe43a32873000c36e8dc1f")
+        // x: field_element!("01ef15c18599971b7beced415a40f0c7deacfd9b0d1819e03d723d8bc943cfca"),
+        // y: field_element!("005668060aa49730b7be4801df46ec62de53ecd11abe43a32873000c36e8dc1f")
 
-        
+        // x: field_element!("0759ca09377679ecd535a81e83039658bf40959283187c654c5416f439403cf5"),
+        // y: field_element!("06f524a3400e7708d5c01a28598ad272e7455aa88778b19f93b562d7a9646c41"),
+
+        // x: field_element!("a7da05a4d664859ccd6e567b935cdfbfe3018c7771cb980892ef38878ae9bc"),
+        // y: field_element!("0584b0c2bc833a4c88d62b387e0ef868cae2eaaa288f4ca7b34c84b46ca031b6"),
+
+        // x: field_element!("06eeee2b0c71d681692559735e08a2c3ba04e7347c0c18d4d49b83bb89771591"),
+        // y: field_element!("072498c69f16e02231e26a6a6acaabb8714e0af1306066231dd38c233ee15216"),
+
+        // x: field_element!("b582a82e6c8ad99e38fcbd2a4da97b37d0cdb7d776edb84a661d79ec4824ac"),
+        // y: field_element!("07cf4349849204906e1b19538552fcb1565171ab453694b574c87e2a35206d9c"),
+
+        // x: field_element!("045f571e26cc38d3e6fdce1b659350f93e272cac9834b78eb8681f7ef89239aa"),
+        // y: field_element!("05abd8de04bd2c3b7890082929fcd2e1aa252e9508f39e19637456eb1d7cc493"),
+
+        // x: field_element!("06c4d7eb804667b5c86b09dd6ad31a554a0f4798cb529eed1615c8c1666cff8e"),
+        // y: field_element!("0605aa7ed522042afe5c9070a71cdf26510758ef847fa4600e7952ae7ec5d536"),
+
+        // x: field_element!("03455359ce0517acec33aed439350e69dd854d6c689dcce27285e5e262806abc"),
+        // y: field_element!("03049f60eb15d1da1c32fc196d0ca814fae84bc3a6890716edbbdbe1038e35ea"),
+
+        x: field_element!("06d840e6d0ecfcbcfa83c0f704439e16c69383d93f51427feb9a4f2d21fbe075"),
+        y: field_element!("058f7ce5eb6eb5bd24f70394622b1f4d2c54ebca317a3e61bf9f349dccf166cf"),
     };
+
+
     info!("Claim: {:?}", claim);
 
-    // info!("Constructing witness");
-    // let witness = Witness {
-    //     secret: field_element!("cafebabe"),
-    // };
-    // info!("Witness: {:?}", witness);
+    //only a "dummy" witness, FieldElement::zero()
 
     assert_eq!(claim.check(&FieldElement::zero()), Ok(()));
 
